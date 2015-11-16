101d1730314926157d06281dc25a165d64ec6cb1
LeeTZ <litiezheng513@gmail.com>
2015-11-16 05:37:51 -0500

    update sast and semantic from the south

--- src/sast.ml
+++ src/sast.ml
@@ -1,5 +1,4 @@
 open Ast
-open Common
 
 type action = 
 	| NewVar
@@ -37,17 +36,25 @@ type s_stmt =
   | SContinue 
   | SBreak 
   | SReturn of s_expr option
-and
-  s_cond_exec = 
+
+and s_cond_exec = 
    SCondExec of s_expr option * s_stmt list
 
 type s_var_decl = 
-	| SVarDecl of string * string
+	| SVarDecl of string * sem
 
-type s_type_mem_decl = 
-	| SMemVarDecl of string * string
-	| SFuncDecl of string * s_var_decl list * s_stmt list
-	|	STypeDecl of string * s_type_mem_decl list
- 
-type s_program = 
-	| SProgram of s_type_mem_decl list
+and s_func_decl = 
+  | SFuncDecl of string * s_var_decl list * s_stmt list * sem
+
+and s_type_decl = 
+  | STypeDecl of string * s_type_mem_decl list
+
+and s_type_mem_decl = 
+  | SMemVarDecl of s_var_decl
+  | SMemFuncDecl of s_func_decl
+  | SMemTypeDecl of s_type_decl
+
+and global_ele_decl = 
+  | SGlobalStmt of s_stmt
+  | SGlobalFunc of s_func_decl
+  | SGlobalType of s_type_decl

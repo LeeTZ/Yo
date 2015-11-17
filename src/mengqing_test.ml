17d05eb62e1df330b704f9ff2af9196cc80dcf71
LeeTZ <litiezheng513@gmail.com>
2015-11-17 02:56:15 -0500

    we made a great progess

--- src/mengqing_test.ml
+++ src/mengqing_test.ml
@@ -2,7 +2,7 @@
 let _ =
 	let lexbuf = Lexing.from_channel stdin in
 	let program = Parser.global Scanner.token lexbuf in
-	let typetab = Type_reader.walk_dec program in 
-	let seman = Semantic.build_semantic typetab program in
+	let context = Built_in_type.generate_built_in_types in 
+	let seman = Semantic.build_semantic context program in
 	let listing = Sast.string_of_s_program seman
 	in print_string listing

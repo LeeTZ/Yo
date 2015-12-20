97bf2e780bbf0aef11aa1b76a5f5c54fe32241eb
Mengqing Wang <mengqwang@dyn-160-39-234-226.dyn.columbia.edu>
2015-12-19 20:39:53 -0500

    print type

--- src/print_typetab.ml
+++ src/print_typetab.ml
@@ -1,16 +1,16 @@
 open Ast
 
-let rec print_typeentry = function
-	| BaseTypeEntry te -> "BaseType" 
-	| ArrayTypeEntry bs -> "Array"
+let rec print_typeentry curtype = function
+	| BaseTypeEntry(be) -> (curtype ^ be.t_name)
+	| ArrayTypeEntry(ae) -> (print_typeentry (curtype^"array of ") ae)
 
-and print_varentry k ve = 
-	print_string ("v_name: " ^ ve.v_name ^ ", v_actual: " ^ ve.v_actual);
-	print_typeentry ve.v_type;
+and print_varentry k = 
+	print_string("v_name: " ^ k.v_name ^ ", v_actual: " ^ k.v_actual);
+	print_string (print_typeentry "" k.v_type);
+	print_string "\n";
 
 and print_memberentry k v = 
 	print_string ("Key: "^k^"\n");
-
 
 and print_basetype k (bt:base_type) =
 	print_string ("Typetab KEY: " ^ k ^ "\n");
@@ -24,9 +24,9 @@ and print_basetype k (bt:base_type) =
 
 and print_evalentry ee = 
 	print_string "args: ";
-	List.iter (fun x -> print_varentry x;()) ee.args;
+	List.iter (fun k -> print_varentry k;()) ee.args;
 	print_string "ret: ";
-	print_string (print_typeentry ee.ret);
+	print_string (print_typeentry "" ee.ret);
 	print_string "\n";
 
 and printtypetab t =

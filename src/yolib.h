29bbe90d09164dbb7e74cf8cf591f2209f1f4fde
Edward Cheng <ecsark@gmail.com>
2015-12-20 19:17:03 -0500

    modify youlib

--- src/yolib.h
+++ src/yolib.h
@@ -2,8 +2,10 @@
 #include <iostream>
 
 struct Universal {};
+using std::string;
+using std::tr1::shared_ptr;
 
-std::shared_ptr<Universal> DUMMY_SELF;
+shared_ptr<Universal> DUMMY_SELF;
 
 template <typename T>
 void LOG (std::shared_ptr<Universal> obj, T str) {
@@ -11,3 +13,13 @@ void LOG (std::shared_ptr<Universal> obj, T str) {
 }
 
 
+struct Clip : Universal {
+
+	static shared_ptr<Clip> eval(string fileName) {
+		return createClip(filename);
+	}
+
+	static void save(shared_ptr<Clip> clip, string fileName) {
+		return writeClips(clip, filename);
+	}
+};
